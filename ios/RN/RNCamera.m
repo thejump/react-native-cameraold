57584947d98658f7ad839c643589c13c50369f61
jcharbo <jcharbo@users.noreply.github.com>
2018-11-16 11:37:50 -0600

    Update RNCamera.m

--- ios/RN/RNCamera.m
+++ ios/RN/RNCamera.m
@@ -971,7 +971,7 @@ static NSDictionary *defaultFaceDetectorOptions = nil;
     [videoComposition setInstructions:@[instruction]];
     
     // Export
-    AVAssetExportSession* exportSession = [AVAssetExportSession exportSessionWithAsset:videoAsset presetName:AVAssetExportPreset1280x720];
+    AVAssetExportSession* exportSession = [AVAssetExportSession exportSessionWithAsset:videoAsset presetName:AVAssetExportPreset640x480];
     NSString* filePath = [RNFileSystem generatePathInDirectory:[[RNFileSystem cacheDirectoryPath] stringByAppendingString:@"converted"] withExtension:@".mp4"];
     NSURL* outputURL = [NSURL fileURLWithPath:filePath];
     [exportSession setOutputURL:outputURL];
